69ef024907b339179cf1ac8b6e661b282d46d5d3
Stayer-svs <svs.souz@gmail.com>
2025-04-04 02:45:00 +0300

    add new cod

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
+
 icon = pygame.image.load("img/4.png")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
+
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
@@ -36,6 +38,5 @@ while running:
 
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
-
 
 pygame.quit()
