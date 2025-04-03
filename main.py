52ea1741ef82712d332a5964cd01ae1d150440ed
Stayer-svs <svs.souz@gmail.com>
2025-04-04 02:50:39 +0300

    refresh code

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-
 icon = pygame.image.load("img/4.png")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
-
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
@@ -40,3 +38,4 @@ while running:
     pygame.display.update()
 
 pygame.quit()
+
